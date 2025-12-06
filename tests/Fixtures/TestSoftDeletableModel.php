2a4d0be37bae219a597d3b5fba44933021363e2a
Helge Sverre <helge.sverre@gmail.com>
2025-12-06 05:32:19 +0100

    test(fixtures): add TestSoftDeletableModel

---
+++ tests/Fixtures/TestSoftDeletableModel.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HelgeSverre\Prunekeeper\Tests\Fixtures;
+
+use HelgeSverre\Prunekeeper\ArchivePrunedRecords;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TestSoftDeletableModel extends Model
+{
+    use ArchivePrunedRecords;
+    use Prunable;
+    use SoftDeletes;
+
+    protected $table = 'test_soft_deletable_models';
+
+    protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'metadata' => 'array',
+        ];
+    }
+
+    /**
+     * Get the prunable model query.
+     *
+     * @return Builder<self>
+     */
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->subMonth());
+    }
+}
